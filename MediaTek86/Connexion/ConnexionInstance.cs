dec457525d0d86dee6d85e8f2d9bb1c9f6e21c45
D3vi73cK <deviteck.injector@gmail.com>
2021-06-04 14:33:50 +0200

    Add Connexion singleton Add Data Access Layer Add mysql nupkg

---
+++ MediaTek86/Connexion/ConnexionInstance.cs
@@ -0,0 +1,151 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Mediatek86.Connexion
+{
+    public class ConnexionInstance
+    {
+        private static ConnexionInstance instance = null;
+
+        private readonly MySqlConnection connexion;
+
+        private MySqlDataReader reader;
+
+        private ConnexionInstance(string stringConnect)
+        {
+            try
+            {
+                connexion = new MySqlConnection(stringConnect);
+                connexion.Open();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Application.Exit();
+            }
+        }
+
+        /// <summary>
+        /// Static method to return the unique instance of the connexion to the database
+        /// </summary>
+        /// <param name="stringConnect">connexion string to the database</param>
+        /// <returns>instance of the connexion to the data base</returns>
+        public static ConnexionInstance GetInstance(string stringConnect)
+        {
+            if (instance is null)
+            {
+                instance = new ConnexionInstance(stringConnect);
+            }
+            return instance;
+        }
+
+        /// <summary>
+        /// Method to execute all request except select
+        /// </summary>
+        /// <param name="stringQuery">query stored in a string</param>
+        /// <param name="parameters">parameter of the request</param>
+        public void ReqUpdate(string stringQuery, Dictionary<string, object> parameters)
+        {
+            try
+            {
+                MySqlCommand command = new MySqlCommand(stringQuery, connexion);
+                if (!(parameters is null))
+                {
+                    foreach (KeyValuePair<string, object> parameter in parameters)
+                    {
+                        command.Parameters.Add(new MySqlParameter(parameter.Key, parameter.Value));
+                    }
+                }
+                command.Prepare();
+                command.ExecuteNonQuery();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        /// <summary>
+        /// Method to execute a request select
+        /// </summary>
+        /// <param name="stringQuery">query stored in a string</param>
+        /// <param name="parameters">parameter of the request</param>
+        public void ReqSelect(string stringQuery, Dictionary<string, object> parameters)
+        {
+            try
+            {
+                MySqlCommand command = new MySqlCommand(stringQuery, connexion);
+                if (!(parameters is null))
+                {
+                    foreach (KeyValuePair<string, object> parameter in parameters)
+                    {
+                        command.Parameters.Add(new MySqlParameter(parameter.Key, parameter.Value));
+                    }
+                }
+                command.Prepare();
+                reader = command.ExecuteReader();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        /// <summary>
+        /// Check if the cursor can read another tuple
+        /// </summary>
+        /// <returns>true if the cursor can read another tuple in Field</returns>
+        public bool Read()
+        {
+            if (reader is null)
+            {
+                return false;
+            }
+            try
+            {
+                return reader.Read();
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Create the field containing all the tuples corresponding to the request
+        /// </summary>
+        /// <param name="nameField">field (string) passed into parameter</param>
+        /// <returns>Returns the field containing all the tuples or null</returns>
+        public object Field(string nameField)
+        {
+            if (reader is null)
+            {
+                return null;
+            }
+            try
+            {
+                return reader[nameField];
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Close the cursor once the field is empty
+        /// </summary>
+        public void Close()
+        {
+            if (!(reader is null))
+            {
+                reader.Close();
+            }
+        }
+    }
+}
---
+++ MediaTek86/DAL/DBAccess.cs
@@ -0,0 +1,409 @@
+﻿using Mediatek86.Connexion;
+using MediaTek86.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediaTek86.DAL
+{
+    public static class DBAccess
+    {
+        private static readonly string connexionString = "server=localhost;user id=dbuser;password=knatasha;database=mediatek86;SslMode=none";
+
+        /// <summary>
+        /// Method to control the authentification
+        /// </summary>
+        /// <param name="login">login of the connexion</param>
+        /// <param name="password">password of the connexion</param>
+        /// <returns>true if the connexion is open, false otherwise</returns>
+        public static Boolean ControlAuthentification(string login, string password)
+        {
+            string req = "select * from responsable ";
+            req += "where login=@login and pwd=SHA2(@pwd, 256);";
+            Dictionary<string, object> parameters = new Dictionary<string, object>
+            {
+                { "@login", login },
+                { "@pwd", password }
+            };
+            ConnexionInstance cursor = ConnexionInstance.GetInstance(connexionString);
+            cursor.ReqSelect(req, parameters);
+            if (cursor.Read())
+            {
+                cursor.Close();
+                return true;
+            }
+            else
+            {
+                cursor.Close();
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Get the list of all employees
+        /// </summary>
+        /// <returns>List of employees in the database</returns>
+        public static List<Employee> GetEmployees()
+        {
+            List<Employee> ListEmployee = new List<Employee>();
+            string req = "SELECT p.idpersonnel as idpersonnel, p.nom as nom, p.prenom as prenom, p.tel as tel, p.mail as mail, s.idservice as idservice, s.nom as service ";
+            req += "from personnel p join service s on (p.idservice = s.idservice) ";
+            req += "order by nom, prenom;";
+            ConnexionInstance cursor = ConnexionInstance.GetInstance(connexionString);
+            cursor.ReqSelect(req, null);
+            while (cursor.Read())
+            {
+
+                Employee employee = new Employee((int)cursor.Field("id"),
+                                                 (string)cursor.Field("nom"),
+                                                 (string)cursor.Field("prenom"),
+                                                 (string)cursor.Field("tel"),
+                                                 (string)cursor.Field("mail"),
+                                                 (int)cursor.Field("idservice"),
+                                                 (string)cursor.Field("service"));
+                ListEmployee.Add(employee);
+            }
+            cursor.Close();
+            return ListEmployee;
+        }
+
+        /// <summary>
+        /// Get the maximum employee ID from the employee in the database
+        /// </summary>
+        /// <returns>integer (maximum ID of the employee) in the table EMPLOYEE</returns>
+        public static int GetMaxEmployeeID()
+        {
+            string req = "select max(id) from personnel";
+            ConnexionInstance cursor = ConnexionInstance.GetInstance(connexionString);
+            cursor.ReqSelect(req, null);
+            int max = 0;
+            if (cursor.Read())
+            {
+                max = (int)cursor.Field("max(id)");
+            }
+            cursor.Close();
+            return max;
+        }
+
+        /// <summary>
+        /// Add an employee to the database
+        /// </summary>
+        /// <param name="employee">Employee (object) from the data selected by user</param>
+        public static void AddEmployee(Employee employee)
+        {
+            string req = "insert into personnel(idpersonnel, idservice, nom, prenom, tel, mail) ";
+            req += "values (@idpersonnel, @idservice, @nom, @prenom, @tel, @mail);";
+            Dictionary<string, object> parameters = new Dictionary<string, object>
+            {
+                {"@idpersonnel", employee.IdEmployee },
+                {"@nom", employee.FamilyName },
+                {"@prenom", employee.FirstName },
+                {"@tel", employee.Phone },
+                {"@mail", employee.Mail },
+                {"@idservice", employee.IdDepartment },
+
+            };
+
+            ConnexionInstance cursor = ConnexionInstance.GetInstance(connexionString);
+            cursor.ReqUpdate(req, parameters);
+        }
+
+        /// <summary>
+        /// Update employee data in the database
+        /// </summary>
+        /// <param name="employee">Employee (object) from the data selected by user</param>
+        public static void UpdateEmployee(Employee employee)
+        {
+            string req = "update personnel set nom = @nom, prenom = @prenom, tel = @tel, mail = @mail, idservice = @idservice ";
+            req += "where idpersonnel = @idpersonnel;";
+            Dictionary<string, object> parameters = new Dictionary<string, object>
+            {
+                {"@idpersonnel", employee.IdEmployee },
+                {"@idservice", employee.IdDepartment },
+                {"@nom", employee.FamilyName },
+                {"@prenom", employee.FirstName },
+                {"@tel", employee.Phone },
+                {"@mail", employee.Mail }
+            };
+            ConnexionInstance cursor = ConnexionInstance.GetInstance(connexionString);
+            cursor.ReqUpdate(req, parameters);
+        }
+
+
+        /// <summary>
+        /// Remove employee absence from the table ABSENCE
+        /// </summary>
+        /// <param name="employee">Employee (object) from the data selected by user</param>
+        public static void RemoveEmployeeFromAbsence(Employee employee)
+        {
+            string req = "delete from absence where idpersonnel = @idpersonnelAbsence ";
+            Dictionary<string, object> parameters = new Dictionary<string, object>
+            {
+                {"@idpersonnelAbsence", employee.IdEmployee}
+            };
+            ConnexionInstance cursor = ConnexionInstance.GetInstance(connexionString);
+            cursor.ReqUpdate(req, parameters);
+        }
+
+        /// <summary>
+        /// Remove employee from the table EMPLOYEE
+        /// </summary>
+        /// <param name="employee">Employee (object) from the data selected by user</param>
+        public static void RemoveEmployeeFromEmployee(Employee employee)
+        {
+            string req = "delete from personnel where idpersonnel = @idpersonnelPersonnel;";
+            Dictionary<string, object> parameters = new Dictionary<string, object>
+            {
+                {"@idpersonnelPersonnel", employee.IdEmployee}
+            };
+            ConnexionInstance cursor = ConnexionInstance.GetInstance(connexionString);
+            cursor.ReqUpdate(req, parameters);
+        }
+
+        /// <summary>
+        /// Get the list of departments stored in the database
+        /// </summary>
+        /// <returns>List of departments</returns>
+        public static List<Department> GetTheDepartments()
+        {
+            List<Department> theDepartments = new List<Department>();
+            string req = "select * from service order by nom;";
+            ConnexionInstance cursor = ConnexionInstance.GetInstance(connexionString);
+            cursor.ReqSelect(req, null);
+            while (cursor.Read())
+            {
+                Department department = new Department((int)cursor.Field("idservice"),
+                                                       (string)cursor.Field("nom"));
+                theDepartments.Add(department);
+            }
+            cursor.Close();
+            return theDepartments;
+        }
+
+        /// <summary>
+        /// Get the list of absences stored in the database
+        /// </summary>
+        /// <param name="employee">Employee (object) from the data selected by user</param>
+        /// <returns>List of absences from an employee</returns>
+        public static List<Absence> GetTheAbsences(Employee employee)
+        {
+            List<Absence> theAbsences = new List<Absence>();
+            string req = "select a.idpersonnel as idpersonnel, a.datedebut as datedebut, m.idmotif as idmotif, a.datefin as datefin, m.libelle as libelle ";
+            req += "from absence a join motif m using (idmotif) ";
+            req += "where a.idpersonnel = @idpersonnel ";
+            req += "order by datedebut DESC";
+            Dictionary<string, object> parameters = new Dictionary<string, object>
+            {
+                {"@idpersonnel", employee.IdEmployee}
+            };
+            ConnexionInstance cursor = ConnexionInstance.GetInstance(connexionString);
+            cursor.ReqSelect(req, parameters);
+            while (cursor.Read())
+            {
+                Absence absence = new Absence(employee,
+                                               (DateTime)cursor.Field("datedebut"),
+                                               (DateTime)cursor.Field("datefin"),
+                                               (int)cursor.Field("idmotif"),
+                                               (string)cursor.Field("libelle")
+                                               );
+                theAbsences.Add(absence);
+            }
+            cursor.Close();
+            return theAbsences;
+        }
+
+        /// <summary>
+        /// Get the list of absence reasons stored in the database
+        /// </summary>
+        /// <returns>List of absence reasons</returns>
+        public static List<Reason> GetTheReasons()
+        {
+            List<Reason> theReasons = new List<Reason>();
+            string req = "select * from motif ";
+            ConnexionInstance cursor = ConnexionInstance.GetInstance(connexionString);
+            cursor.ReqSelect(req, null);
+            while (cursor.Read())
+            {
+                Reason reason = new Reason((int)cursor.Field("idmotif"),
+                                           (string)cursor.Field("libelle"));
+                theReasons.Add(reason);
+            }
+            cursor.Close();
+            return theReasons;
+        }
+
+        /// <summary>
+        /// Add an absence to an employee
+        /// </summary>
+        /// <param name="employee">Employee (object) from the data selected by user</param>
+        /// <param name="firstDay">first day of the absence</param>
+        /// <param name="lastDay">last day of the absence</param>
+        /// <param name="idReason">ID reason of the absence</param>
+        public static void AddAbsence(Employee employee, DateTime firstDay, DateTime lastDay, int idReason)
+        {
+            string req = "insert into absence(idpersonnel, datedebut, idmotif, datefin) ";
+            req += "values (@idpersonnel, @datedebut, @idmotif, @datefin) ";
+            Dictionary<string, object> parameters = new Dictionary<string, object>
+            {
+                {"@idpersonnel", employee.IdEmployee },
+                {"@datedebut", firstDay },
+                {"@idmotif", idReason },
+                {"@datefin", lastDay }
+            };
+
+            ConnexionInstance cursor = ConnexionInstance.GetInstance(connexionString);
+            cursor.ReqUpdate(req, parameters);
+        }
+
+        /// <summary>
+        /// Update absence from an employee
+        /// </summary>
+        /// <param name="employee">Employee (object) from the data selected by user</param>
+        /// <param name="previousDateSelected">first day, part of the composite primary key absence</param>
+        /// <param name="firstDay">first day selected</param>
+        /// <param name="lastDay">last day selected</param>
+        /// <param name="idReason">ID reason of the absence</param>
+        public static void UpdateAbsence(Employee employee,
+                                         DateTime previousDateSelected,
+                                         DateTime firstDay,
+                                         DateTime lastDay,
+                                         int idReason
+                                         )
+        {
+            string req = "update absence set datedebut = @datedebut, datefin = @datefin, idmotif = @idmotif ";
+            req += "where idpersonnel = @idpersonnel and datedebut = @datedebutAvantMAJ;";
+            Dictionary<string, object> parameters = new Dictionary<string, object>
+            {
+                { "@idpersonnel", employee.IdEmployee },
+                { "@datedebut", firstDay },
+                { "@datedebutAvantMAJ", previousDateSelected },
+                { "@datefin", lastDay },
+                { "@idmotif", idReason }
+            };
+
+            ConnexionInstance cursor = ConnexionInstance.GetInstance(connexionString);
+            cursor.ReqUpdate(req, parameters);
+        }
+
+        /// <summary>
+        /// Remove an absence from an employee
+        /// </summary>
+        /// <param name="absence">Absence (object) selected from an employee</param>
+        /// <param name="employee">Employee (object) selected by a user</param>
+        public static void RemoveAbsenceFromEmployee(Absence absence, Employee employee)
+        {
+            string req = "delete from absence ";
+            req += "where idpersonnel = @idpersonnel ";
+            req += "and datedebut = @datedebut";
+            Dictionary<string, object> parameters = new Dictionary<string, object>
+            {
+                {"@idpersonnel", employee.IdEmployee},
+                {"@datedebut", absence.FirstDay}
+            };
+            ConnexionInstance cursor = ConnexionInstance.GetInstance(connexionString);
+            cursor.ReqUpdate(req, parameters);
+        }
+
+        /// <summary>
+        /// Get the last day corresponding to an absence
+        /// </summary>
+        /// <param name="employee">Employee (object) from the data selected by user</param>
+        /// <returns>Last day corresponding to an absence</returns>
+        public static DateTime AbsenceAtTheEndOfTheCalendar(Employee employee)
+        {
+            DateTime max = new DateTime();
+            string req = "SELECT MAX(datefin) ";
+            req += "FROM absence ";
+            req += "WHERE idpersonnel = @idpersonnel";
+
+            Dictionary<string, object> parameters = new Dictionary<string, object>
+            {
+                {"@idpersonnel", employee.IdEmployee},
+            };
+
+            ConnexionInstance cursor = ConnexionInstance.GetInstance(connexionString);
+            cursor.ReqSelect(req, parameters);
+            while (cursor.Read())
+            {
+                max = (DateTime)cursor.Field("max(datefin)");
+
+            }
+            cursor.Close();
+            return max;
+
+        }
+
+        /// <summary>
+        /// Get the first day of the next absence - timeslot in between absences
+        /// </summary>
+        /// <param name="employee">Employee (object) from the data selected by user</param>
+        /// <param name="firstDay">first day selected by user</param>
+        /// <returns>first day of the next absence</returns>
+        public static DateTime LastDayIsBeforeNextAbsence(Employee employee, DateTime firstDay)
+        {
+            DateTime firstDayNextAbsence = new DateTime();
+            string req = "SELECT MIN(datedebut) ";
+            req += "FROM absence ";
+            req += "WHERE idpersonnel = @idpersonnel ";
+            req += "AND datedebut > @firstDayPicked";
+
+            Dictionary<string, object> parameters = new Dictionary<string, object>
+            {
+                {"@idpersonnel", employee.IdEmployee},
+                {"@firstDayPicked", firstDay}
+            };
+
+            ConnexionInstance cursor = ConnexionInstance.GetInstance(connexionString);
+            cursor.ReqSelect(req, parameters);
+            while (cursor.Read())
+            {
+                firstDayNextAbsence = (DateTime)cursor.Field("min(datedebut)");
+
+            }
+            cursor.Close();
+            return firstDayNextAbsence;
+        }
+
+        /// <summary>
+        /// Get the last day of the previous absence - timeslot in between absences
+        /// </summary>
+        /// <param name="employee">Employee (object) from the data selected by user</param>
+        /// <param name="firstDay">first day selected by user</param>
+        /// <returns>last day of the previous absence</returns>
+        public static DateTime FirstDayIsAfterPreviousAbsence(Employee employee,
+                                                              DateTime firstDay
+                                                              )
+        {
+            DateTime lastDayPreviousAbsence = new DateTime();
+            string req = "SELECT MAX(datefin) ";
+            req += "FROM absence ";
+            req += "WHERE idpersonnel = @idpersonnel ";
+            req += "AND datefin < ( ";
+            req += "SELECT MIN(datedebut) ";
+            req += "FROM absence ";
+            req += "WHERE idpersonnel = @idpersonnel ";
+            req += "AND datedebut > @firstDayPicked)";
+
+
+            Dictionary<string, object> parameters = new Dictionary<string, object>
+            {
+                {"@idpersonnel", employee.IdEmployee},
+                {"@firstDayPicked", firstDay}
+            };
+
+            ConnexionInstance cursor = ConnexionInstance.GetInstance(connexionString);
+            cursor.ReqSelect(req, parameters);
+            while (cursor.Read())
+            {
+                lastDayPreviousAbsence = (DateTime)cursor.Field("max(datefin)");
+
+            }
+            cursor.Close();
+            return lastDayPreviousAbsence;
+
+        }
+
+    }
+}
